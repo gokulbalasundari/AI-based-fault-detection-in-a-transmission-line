5ad8365f4a84091839544aa6d1f3b8d61ff46db4
Gokul P <110407011+gokulbalasundari@users.noreply.github.com>
2025-02-18 15:15:25 +0530

    change the url path for root

--- AI_BASED_FAULT_DETECTION/urls.py
+++ AI_BASED_FAULT_DETECTION/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fault/',include('fault_detection.urls'))
+    path('',include('fault_detection.urls'))
 ]
--- fault_detection/urls.py
+++ fault_detection/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import*
 
 urlpatterns =[
-    path('input/',inputform),
+    path('',inputform),
     path('result/',result)
 
 ]
